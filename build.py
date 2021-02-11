e00076e4b4e7360940abd2a5cf1c27ee388c3b8d
Lázaro Menezes <lazarodm@gmail.com>
2021-02-10 22:15:00 -0300

    Complete build script features

--- build.py
+++ build.py
@@ -1,27 +1,39 @@
-class Post:
-    
-    def __init__(self):
-        self.title_pt = ""
-        self.title_en = ""
-        self.url_pt = ""
-        self.url_en = ""
+import requests
+import xml.etree.ElementTree as et
 
-    def toMdTableRow(self):
-        return "| [{0}]({1}) | [{2}]({3}) |".format(self.title_pt, self.url_pt, self.title_en, self.url_en)
+def from_items(pt_item, en_item):
+
+    title_pt = pt_item.find('title').text
+    title_en = en_item.find('title').text
+    url_pt = pt_item.find('link').text
+    url_en = en_item.find('link').text
+
+    return "| [{0}]({1}) | [{2}]({3}) |".format(title_pt, url_pt, title_en, title_pt)
+
+def load_rss_items(url):
+    data = requests.get(url)
+    data.encoding = 'utf-8'
+
+    root = et.fromstring(data.text)
+
+    return root.findall('./channel/item')
 
 if __name__ == '__main__':
-
     with open('README.md.template', 'r') as template_file:
         template = template_file.read()
 
-    post = Post()
-    post.title_pt = "OI"
-    post.title_en = "HI"
-    post.url_pt = "http://oi"
-    post.url_en = "http://hi"
+    rss_pt = 'http://www.lazarodm.com.br/index.xml'
+    rss_en = 'http://www.lazarodm.com.br/en/index.xml'
 
-    result = template.replace('{{POSTS}}', post.toMdTableRow())
+    pt_items = load_rss_items(rss_pt)
+    en_items = load_rss_items(rss_en)
 
-    print(result)
+    posts = []
+
+    for i in range(len(pt_items)):
+        posts.append(from_items(pt_items[i], en_items[i]))
+
+    result = "\n".join(posts)
+    print(template.replace("{{POSTS}}", result))
 
 
